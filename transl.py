cb96e7de03505dc1a7969f8ae0274b8e8bcc088a
Pontus Hylander <pontus.hylander@edu.edugrade.se>
2025-03-23 11:42:39 +0100

    second

---
+++ transl.py
@@ -0,0 +1,11 @@
+from translate import Translator
+translator = Translator(to_lang='sv')  # Spanish
+
+# Text to be translated
+text = 'Hello, how are you?'
+
+# Perform the translation
+translation = translator.translate(text)
+
+# Print the translated text
+print('Translated Text:', translation)
